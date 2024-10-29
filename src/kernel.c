816e63843e9b5bb2c54f19655e66c08457287e44
wangyanyo <2378130965@qq.com>
2024-10-29 16:34:15 +0800

    Implemented low level disk functionality in C

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* vedio_mem = 0;
 uint16_t terminal_row = 0;
@@ -71,19 +72,12 @@ void kernel_main() {
     // 可写，有效，全优先级可读
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PERSENT | PAGING_ACCESS_FROM_ALL);
 
-    void* ptr = kzalloc(4096);
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PERSENT | PAGING_IS_WRITEABLE);
-
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
     enable_paging();
 
-    char* ptr2 = (char*)0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-
-    print(ptr2);
-    print(ptr);
+    char buf[512];
+    disk_read_sector(0, 1, buf);
 
     enable_interrupts();
 }

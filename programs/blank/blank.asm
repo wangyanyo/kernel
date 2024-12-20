1ada134737ca5183fef12afa9e979d59f32e0256
wangyanyo <2378130965@qq.com>
2024-12-20 22:36:43 +0800

    review and summary

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -8,22 +8,29 @@ _start:
 
 label:
 
-_loop:
-    call getkey
+; _loop:
+;     call getkey
 
-    push eax 
-    mov eax, 3  ; Command 3: putchar
+;     push eax 
+;     mov eax, 3  ; Command 3: putchar
+;     int 0x80
+;     add esp, 4
+
+;     jmp _loop
+
+; getkey:
+;     mov eax, 2
+;     int 0x80
+;     cmp eax, 0x00
+;     je getkey
+;     ret
+
+    push message
+    mov eax, 1
     int 0x80
     add esp, 4
-
-    jmp _loop
-
-getkey:
-    mov eax, 2
-    int 0x80
-    cmp eax, 0x00
-    je getkey
-    ret
+    
+    jmp $
 
 section .data
 message: db 'I can talk with kernel!', 0
